e2b1175c181da14254d769306f6c74e5626ae6dc
Mai Baba <baba.smartnext@gmail.com>
2022-02-04 10:45:55 +0900

    データーベースの導入

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 
 // CSS読込み
 app.use(express.static('public'));
+
+// MySQLの接続
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Baba0814',
+  database: 'list_app'
+});
 
 // トップ画面
 app.get('/', (req, res) => {
